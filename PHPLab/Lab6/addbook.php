a6eecd43f192529439334d261bea446e5917b751
shawonsys <tawhiduzzamans@gmail.com>
2020-11-10 16:51:46 +0600

    lab 6 2nd commit

---
+++ PHPLab/Lab6/addbook.php
@@ -0,0 +1,75 @@
+<html>
+	<head>
+		<title>AddBook</title>
+	</head>
+	<body>
+		
+		<h1>Add Book</h1>
+		<form action="" method="post">
+			
+				<table>
+					<tr>
+						<td align="left">Book Name:</td>
+					</tr>
+					<tr>
+						<td><input type="text" name="bname"></td>
+					</tr>
+					<tr>
+						<td align="left">Category</td>
+					</tr>
+					<tr>
+						<td>
+							<select>
+								<option>Engineering</option>
+								<option>Architectuure</option>
+								<option>Novel</option>
+													
+							</select>
+						</td>
+					</tr>
+					
+					<tr>
+						<td>Description</td>
+					</tr>
+					<tr>
+						<td><textarea></textarea></td>
+					</tr>
+					<tr>
+						<td align="left">Publisher:</td>
+					</tr>
+					<tr>
+						<td><input type="text" name="publisher"></td>
+					</tr>
+					<tr>
+						<td align="left">Edition:</td>
+					</tr>
+					<tr>
+						<td><input type="text" name="edition"></td>
+					</tr>
+					<tr>
+						<td align="left">ISBN:</td>
+					</tr>
+					<tr>
+						<td><input type="text" name="isbn"></td>
+					</tr>
+					<tr>
+						<td align="left">Pages:</td>
+					</tr>
+					<tr>
+						<td><input type="text" name="pages"></td>
+					</tr>
+					<tr>
+						<td align="left">Price:</td>
+					</tr>
+					<tr>
+						<td><input type="text" name="price"></td>
+					</tr>
+					<tr>
+						<td><input type="submit" name="submit" value="submit"></td>
+					</tr>
+				</table>
+			
+		</form>
+	</body>
+	
+</html>
--- PHPLab/Lab6/dashboard.php
+++ PHPLab/Lab6/dashboard.php
@@ -11,7 +11,22 @@
 						<td colspan="7" align="left"><a href="addbook.php">Add New Book</a></td>
 					</tr>
 					<tr>
-						
+						<td><u>SR. NO</u></td>
+						<td><u>NAME</u></td>
+						<td><u>PUBLISHER</u></td>
+						<td><u>ISBN</u></td>
+						<td><u>PRICE</u></td>
+						<td><u>IMAGE</u></td>
+						<td><u>DELETE</u></td>
+					</tr>
+					<tr>
+						<td></td>
+					</tr>
+					<tr>
+						<td></td>
+					</tr>
+					<tr>
+						<td></td>
 					</tr>
 					
 				</table>
